7bfd6910a955a786962e5bc23bd434db51c012dd
Jason Hussey <jphussey05@gmail.com>
2025-12-01 22:30:34 -0500

    refactor movement logic to handle rotations more explicitly

--- day01a.py
+++ day01a.py
@@ -6,14 +6,23 @@ with open(FILEPATH) as fin:
 pos = 50
 cnt = 0
 
+
 for rot in contents:
     change = int(rot[1:])
 
     if rot[0] == 'L':
-        change = 100 - (change % 100)
+        for _ in range(change):
+            pos -= 1
+            if pos == 0:
+                cnt += 1
+            elif pos == -1:
+                pos = 99
+    elif rot[0] == 'R':
+        for _ in range(change):
+            pos += 1
+            if pos == 100:
+                cnt += 1
+                pos = 0
 
-    pos = (pos + change) % 100
-    if pos == 0:
-        cnt += 1
 
 print(cnt)
