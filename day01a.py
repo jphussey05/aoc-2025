b85369e503f3d9c0b44484a06f3dcc2541701b36
Jason Hussey <jphussey05@gmail.com>
2025-12-01 22:32:46 -0500

    simplify movement logic by consolidating left and right rotations

--- day01a.py
+++ day01a.py
@@ -10,19 +10,16 @@ cnt = 0
 for rot in contents:
     change = int(rot[1:])
 
-    if rot[0] == 'L':
-        for _ in range(change):
-            pos -= 1
-            if pos == 0:
-                cnt += 1
-            elif pos == -1:
-                pos = 99
-    elif rot[0] == 'R':
-        for _ in range(change):
-            pos += 1
-            if pos == 100:
-                cnt += 1
-                pos = 0
+    modifier = 1 if rot[0] == 'R' else -1
+    for _ in range(change):
+        pos += modifier
+        if pos == 0:
+            cnt += 1
+        elif pos == -1:
+            pos = 99
+        elif pos == 100:
+            cnt += 1
+            pos = 0
 
 
 print(cnt)
